bfaac621608abee72d2f85936960694a3c382350
JadFu <yfu5@aceproject.space>
2023-09-07 13:18:09 -0500

    add routers to handle search
    at routers which takes year and month to  route user to ...slug page

--- components/events/event_search.js
+++ components/events/event_search.js
@@ -1,19 +1,32 @@
+import { useRef } from "react";
 import Button from "../ui/button";
 import css from "./event_search.module.css";
 
 export default function SearchEvent(props){
-    return <form className={css.form}>
+    const yearRef = useRef();
+    const monthRef = useRef();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const selectedYear = yearRef.current.value;
+        const selectedMonth = monthRef.current.value;
+
+        props.onSearch(selectedYear,selectedMonth);
+
+    }
+
+    return <form className={css.form} onSubmit={handleSubmit}>
         <div className={css.controls}>
             <div className={css.control}>
                 <label htmlFor="year">Year</label>
-                <select id='year'>
+                <select id='year' ref={yearRef}>
                     <option value='2021'>2021</option>
                     <option value='2022'>2022</option>
                 </select>
             </div>
             <div className={css.control}>
                 <label htmlFor="month">Month</label>
-                <select id='month'>
+                <select id='month' ref={monthRef}>
                     <option value='1'>January</option>
                     <option value='2'>February</option>
                     <option value='3'>March</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import EventList from "@/components/events/event_list";
 import SearchEvent from "@/components/events/event_search";
 import { getAllEvents } from "@/dummy_events";
+import { useRouter } from "next/router";
 
 export default function AllEvents() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function handleSearch(year, month){
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <div>
-      <SearchEvent />
+      <SearchEvent onSearch={handleSearch}/>
       <EventList events={events}/>
     </div>
   );
