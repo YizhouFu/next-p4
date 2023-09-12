70c0e6747d418d655121bb8c52c790ae937e7fcc
JadFu <yfu5@aceproject.space>
2023-09-12 14:36:54 -0500

    improving all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,21 +1,22 @@
 import EventList from "@/components/events/event_list";
 import SearchEvent from "@/components/events/event_search";
-import { getAllEvents } from "@/dummy_events";
+import { getAllEvents } from "@/helpers/api-util";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 
 export default function AllEvents() {
   const events = getAllEvents();
   const router = useRouter();
 
-  function handleSearch(year, month){
+  function handleSearch(year, month) {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
   }
 
   return (
-    <div>
-      <SearchEvent onSearch={handleSearch}/>
-      <EventList events={events}/>
-    </div>
+    <Fragment>
+      <SearchEvent onSearch={handleSearch} />
+      <EventList events={events} />
+    </Fragment>
   );
 }
