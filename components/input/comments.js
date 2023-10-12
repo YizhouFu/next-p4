6a045fe34a7306dfc39d8c514ab829ce728beb47
JadFu <yfu5@aceproject.space>
2023-10-12 11:54:43 -0500

    Revert "fixing bugs"
    This reverts commit 32fc6ab03d1d0ad34c24c6df88cccddc11b7a014.

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
@@ -8,21 +8,9 @@ function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    if (showComments) {
-      fetch("/api/comments/" + eventId)
-        .then((response) => response.json())
-        .then((data) => {
-          setComments(data.comments);
-        });
-    }
-  }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
-
   }
 
   function addCommentHandler(commentData) {
@@ -44,7 +32,7 @@ function Comments(props) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments}/>}
+      {showComments && <CommentList />}
     </section>
   );
 }
--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -26,12 +26,12 @@ export default function handler(req, res) {
     res.status(201).json({ message: "added comment", comment: newComment });
   }
 
-  if (req.method === "GET") {
+  if ((req, method === "GET")) {
     const dummyList = [
       { id: "c1", name: "Max", comment: "init comment" },
       { id: "c2", name: "Max", comment: "initwo comment" },
     ];
 
-    res.status(200).json({ comments: dummyList });
+    res.status(200), json({ comments: dummyList });
   }
 }
