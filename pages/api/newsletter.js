f9167425c523887a06ca4b2719512370d64d458c
JadFu <yfu5@aceproject.space>
2023-10-12 11:57:45 -0500

    connected with mongo

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -7,7 +9,13 @@ export default function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      "mongodb+srv://next-project:learning@cluster0.ljtn0wq.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+    client.close();
+
     res.status(201).json({ message: "Signed up!" });
   }
 }
